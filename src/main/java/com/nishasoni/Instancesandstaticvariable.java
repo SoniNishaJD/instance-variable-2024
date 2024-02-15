324214b0ae0d1a6390281a36388106648113711f
Soni Nisha <soni.nishaa24@gmail.com>
2024-02-15 06:04:49 +0530

    1st commit 15 feb 2024

---
+++ src/main/java/com/nishasoni/Instancesandstaticvariable.java
@@ -0,0 +1,11 @@
+package com.nishasoni;
+
+//In project when we use Instance and when Static variable give the scenario?
+public class Instancesandstaticvariable {
+    int eid;
+    String ename;
+    double esal;
+//static String company = "tcs"; ====> This is 1 id modifier then all id automatically modifier
+   public static final String company = "tcs"; // Can't modifier
+    public static final String location = "Mumbai";
+}
